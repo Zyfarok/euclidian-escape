b5de9fd33f34992e5827eea97bba81ba30177d0c
Burgelin Clément Louis Alexandre <Zyfarok@gmail.com>
2019-05-14 18:55:42 +0200

    Implement diskette destruction

--- Assets/DisketteCollisionDetector.cs
+++ Assets/DisketteCollisionDetector.cs
@@ -7,16 +7,20 @@ public class DisketteCollisionDetector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerEnter(Collider other) {
-        Debug.Log("Entered !");
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Diskette")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
