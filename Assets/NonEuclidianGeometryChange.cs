14c86899e04dbe21419bcf9369de62e3718ef5be
Clément Burgelin <Zyfarok@gmail.com>
2019-05-16 23:21:16 +0200

    EngineRoom + NonEuclidianGeometryChange Script

---
+++ Assets/NonEuclidianGeometryChange.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonEuclidianGeometryChange : MonoBehaviour
+{
+    private enum Orientation
+    {
+        None = -1, NE, NW, SW, SE
+    }
+
+    
+    GameObject engineRoom = null;
+    GameObject frontWall = null;
+
+    bool isEnabled = true;
+    Orientation lastOrientation = Orientation.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        engineRoom = GameObject.Find("EngineRoom");
+        frontWall = GameObject.Find("CockpitFrontWall");
+        engineRoom.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = VRTK.VRTK_DeviceFinder.HeadsetTransform().rotation.eulerAngles.y;
+        Orientation current = GetOrientation(angle);
+        Debug.Log("Update called: " + angle + "° and " + current);
+        if(!isEnabled || lastOrientation == Orientation.None) {
+            lastOrientation = current;
+            return;
+        }
+
+        if((current == Orientation.SW && lastOrientation == Orientation.SE) ||
+            (current == Orientation.SE && lastOrientation == Orientation.SW)
+        ) {
+            SwapFront();
+        }
+        // Other swap can come later
+
+        lastOrientation = current;
+    }
+
+    private Orientation GetOrientation(float angle) {
+        int i = (int)Mathf.Floor(angle / 90) % 4;
+        if(i < 0) {
+            i += 4;
+        }
+
+        switch(i) {
+        case 0:
+            return Orientation.NW;
+        case 1:
+            return Orientation.SW;
+        case 2:
+            return Orientation.SE;
+        default:
+            return Orientation.NE;
+        }
+    }
+
+    private void SwapFront() {
+        if(engineRoom.activeSelf) {
+            engineRoom.SetActive(false);
+            frontWall.SetActive(true);
+        } else {
+            engineRoom.SetActive(true);
+            frontWall.SetActive(false);
+        }
+    }
+}
