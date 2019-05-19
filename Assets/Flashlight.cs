f54de9ed0c1fc9c4003d54ca7ead0d67f2214059
Clément Burgelin <Zyfarok@gmail.com>
2019-05-19 22:42:46 +0200

    Radio + Flashlight

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VRTK.VRTK_InteractableObject>().InteractableObjectUsed += new VRTK.InteractableObjectEventHandler((sender, args) => {
+            bool newState = !light1.activeSelf; light1.SetActive(newState); light2.SetActive(newState); light3.SetActive(newState);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RadioScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioScript : MonoBehaviour
+{
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VRTK.VRTK_InteractableObject>().InteractableObjectTouched +=
+            new VRTK.InteractableObjectEventHandler((sender, arg) => audio.enabled = !audio.enabled);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
